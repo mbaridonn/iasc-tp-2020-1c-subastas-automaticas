fbbdf377abda3ac912e0ba5ae67dad559178b465
imiandrusic <imiandrusic@cys.com.ar>
2020-07-09 19:22:54 -0300

    Borro variables de mas en router

--- subastas-api/src/router.ts
+++ subastas-api/src/router.ts
@@ -2,9 +2,6 @@ import express from 'express';
 
 export const register = (app: express.Application) => {
 
-    var buyers = buyersModule.buyers;
-    var bids = bidsModule.bids;
-
     app.get('/', function (req, res) {
         res.send('Hello World!');
     });
