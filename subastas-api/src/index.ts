e0ee04ebf460596740c29055a86ddbd84441f95c
Tobias Sandler <tobias.sandler@uala.com.ar>
2020-07-12 19:24:37 -0300

    adding /binds in subastas-api

--- subastas-api/src/index.ts
+++ subastas-api/src/index.ts
@@ -5,11 +5,26 @@ import * as bodyParser from "body-parser";
 // Create a new express app instance
 const app: express.Application = express();
 const port: number = 3000;
+let mainNodes: Array<number>
+let otherNodes: Array<number>
+
+let amountOfNodes = 0
 
 app.use(bodyParser.json());
 
-router.register(app);
+router.register(app, mainNodes, otherNodes);
 
 app.listen(port, function () {
     console.log(`Server started at http://localhost:${port}`);
 });
+
+const initNodes = function () {
+    addNode(mainNodes)
+    addNode(otherNodes)
+    addNode(otherNodes)
+}
+
+const addNode = function (list: Array<number>) {
+    amountOfNodes++
+    list.push(port + amountOfNodes)
+}
--- subastas-api/src/router.ts
+++ subastas-api/src/router.ts
@@ -1,9 +1,36 @@
 import express from 'express';
+// import axios from 'axios'
 
-export const register = (app: express.Application) => {
+export const register = (app: express.Application, mainNodes: Array<number>, otherNodes: Array<number>) => {
 
     app.get('/', function (req, res) {
         res.send('Hello World!');
     });
 
+    app.get('/bids', async function(req, res) {
+        try {
+            let bids = await Promise.all(mainNodes.map(node => nodeBids(node)))
+            res.send(mapBids(bids))
+        }catch(failedNode){
+            replaceNode(failedNode)
+            res.status(400)
+            res.send("error")
+        }
+    })
+
+    const replaceNode = function(failedNode: any){
+        let node = otherNodes.pop()
+        let index = mainNodes.indexOf(failedNode, 0)
+        mainNodes.splice(index, 1)
+        mainNodes.push(node)
+        otherNodes.push(failedNode)
+    }
+}
+
+const mapBids = function (bids: any) {
+    return "bids.flat()"
+}
+
+const nodeBids = function(node: number){
+    // return axios.get(`https://localhost:${node}/bids`)
 }
